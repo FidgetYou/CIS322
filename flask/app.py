bc1003f2c9ca87f8991b67bcf02fef109e758d98
fidgetyou <fidgetyou@gmail.com>
2017-02-05 21:39:31 +0000

    More stub files

---
+++ flask/app.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, redirect, url_for, session, flash
+from CONFIG import HOST, PORT, DEBUG, APP_SECRET_KEY, DB_LOCATION
+from db_helper import UTC_OFFSET
+import psycopg2
+import datetime
+from ast import literal_eval
+from operator import attrgetter
+from picklesession import PickleSessionInterface
+import os
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login')
+def welcome():
+    return render_template('login.html')
+
+@app.route('/logout')
+def goodbye():
+    return render_template('logout.html')
+
+@app.route('/report_filter')
+def report_filter():
+    return render_template('report_filter.html')
+
+@app.route('/report_facility')
+def facilityInventory():
+    return render_template('report_facility.html')
+
+@app.route('/report_transit')
+def InTransit():
+    return render_template('report_transit.html')
+
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
+
