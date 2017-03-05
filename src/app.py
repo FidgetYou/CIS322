7940a4855e94cfc9e5a507a0381457032e1b4a79
FidgetYou <fidgetyou@gmail.com>
2017-03-04 22:14:45 -0800

    forgot to double my =

--- src/app.py
+++ src/app.py
@@ -251,7 +251,7 @@ def logout():
 @app.route('/do_work', methods=['GET'])
 def do_work():
     logisticsOfficer = "Logistics Officer"
-    if session['role'] = logisticsOfficer:
+    if session['role'] == logisticsOfficer:
         return render_template('transfer_req.html')
     else:
         return render_template('transfer_req.html')
@@ -262,7 +262,7 @@ def do_work():
 @app.route('/dashboard', methods=['GET'])
 def dashboard():
     logisticsOfficer = "Logistics Officer"
-    if session['role'] = logisticsOfficer
+    if session['role'] == logisticsOfficer
         SQL = "SELECT asset.asset_tag FROM asset, transit WHERE transit.asset_fk = asset.asset_pk AND (transit.load_time = null OR transit.unload_time = null"
     else:
         SQL = "SELECT asset.asset_tag FROM asset, requests WHERE requests.asset_fk = asset.asset_pk AND requests.approved = false AND requests.rejected = false "
