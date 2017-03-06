eb1a4d8f7e687120036faf5535018800378505d2
FidgetYou <fidgetyou@gmail.com>
2017-03-06 00:54:18 -0800

    Update app.py

--- src/app.py
+++ src/app.py
@@ -341,7 +341,7 @@ def approve_req():
         request_txt = request_txt + "" + ac[0] + "."
         session['request_text'] = request_txt
         
-         return redirect(url_for('approve_req'))
+        return redirect(url_for('approve_req'))
     
     if request.method == 'POST':
         session['error'] = ""
