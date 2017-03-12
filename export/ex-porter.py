a23ee5fca78563f2946b0679d0ea507c0c3567e4
FidgetYou <fidgetyou@gmail.com>
2017-03-11 17:24:45 -0800

    Not sure how to insert 2 username/facility codes

--- export/ex-porter.py
+++ export/ex-porter.py
@@ -11,47 +11,77 @@ import datetime
 from os.path import expanduser
 home = expanduser("~")
 
+export_files = [ 'users.csv', 'facilities.csv', 'assets.csv', 'transfers.csv' ]
+#arg3 = "$HOME/" + sys.argv[3]
+#arg1 = "" + home + sys.argv[1]
 
-arg3 = "./osnap_legacy/" + sys.argv[3]
-arg1 = "" + home + sys.argv[1]
-
-with open(arg3, 'r') as f:
-    reader = csv.reader(f)
-    csv_list = list(reader)
+#with open(arg3, 'r') as f:
+#    reader = csv.reader(f)
+#    csv_list = list(reader)
 
 #print (csv_list)
 
 # Setup the database connection #
 try:
     conn = psycopg2.connect(dbname=sys.argv[1],host='127.0.0.1',port=int(sys.argv[2]))
+    cur = conn.cursor()
 except:
-    print("Unable to connect to the database.")
+    sys.exit("Unable to connect to the database.")
 
-cur = conn.cursor()
+    
+SQL = "SELECT user_name.username, user_name.password, role.role, user_name.active FROM asset, role WHERE asset.role_fk = role.role_pk "
+cur.execute(SQL)
+ac = cur.fetchall()
+#print ("what does a query return = ")
+#print (ac)
+       
+#asset_trsf = []
+#for f in ac:
 
-for i in range (4, len(sys.argv), 3):
-    #print ("i = " + str(i))
-    for j in range (1, len(csv_list)):
-        numb = int(sys.argv[i+2])
-        thing1 = sys.argv[i]
-        thing1 = thing1.replace("`", "")
-        thing1 = thing1.replace("'", "")
+with open(export_files[0], 'w') as out:
+    writer = csv.writer(out)
+    for f in ac:
+        writer.writerow(f)
+out.close()          
+#b = dict()
+#b['asset_name']=f[0]
+#b['facility_name']=f[1]
+#asset_trsf.append(b)
 
-        thing2 = sys.argv[i+1]
-        thing3 = csv_list[j][numb]
+#session['assets_transfer'] = asset_trsf
 
-        #print ("insert into table %s on the row %s this value %s ", (sys.argv[i].replace("'", ""), sys.argv[i+1], csv_list[j][numb], ))    
+SQL = "SELECT facility_code, facility_name FROM facility "
+cur.execute(SQL)
+ac = cur.fetchall()
 
-        curQuery = "INSERT INTO "+thing1+" ("+thing2+") VALUES ('"+thing3+"');"
+with open(export_files[1], 'w') as out:
+    writer = csv.writer(out)
+    for f in ac:
+        writer.writerow(f)
+out.close()  
 
-        try:
-            cur.execute(curQuery)
-            #print ( "Inserted "+thing3+" into table "+thing1)
-        except Exception:
-            print (csv_list[0][numb] + " had no information at entry #" + str(j))
+        
+SQL = "SELECT asset.asset_tag, asset.asset_info, facility.facility_code, asset_at.arrive, asset_at.depart FROM asset, facility, asset_at WHERE asset.asset_pk = asset_at.asset_fk AND asset_at.facility_fk = facility.facility_pk "
+cur.execute(SQL)
+ac = cur.fetchall()
 
- 
-conn.commit()
+with open(export_files[2], 'w') as out:
+    writer = csv.writer(out)
+    for f in ac:
+        writer.writerow(f)
+out.close()  
+
+        
+SQL = "SELECT asset.asset_tag, facility.facility_code, facility_name FROM facility "
+cur.execute(SQL)
+ac = cur.fetchall()
+
+with open(export_files[3], 'w') as out:
+    writer = csv.writer(out)
+    for f in ac:
+        writer.writerow(f)
+out.close()        
+
 
 cur.close()
 conn.close()
